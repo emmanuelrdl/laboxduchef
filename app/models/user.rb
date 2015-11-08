7ca8973dc130a93e8a373dfa8e29c9deed9cbe42
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-11-08 20:29:41 +0100

    home work in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
          omniauth_providers: [:facebook]
 
-  has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+  # has_attached_file :picture,
+  #   styles: { medium: "300x300>", thumb: "100x100>" }
 
-  validates_attachment_content_type :picture,
-    content_type: /\Aimage\/.*\z/
+  # validates_attachment_content_type :picture,
+  #   content_type: /\Aimage\/.*\z/
 
 
 
