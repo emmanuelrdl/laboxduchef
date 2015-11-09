3002f2993490fdeef67f57a42ae2f6bc792dd5fe
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-11-09 18:53:35 +0100

    restaurant new work in progress

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,25 @@
 class RestaurantsController < ApplicationController
 
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
-    @index = Restaurant.all
+    @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(params_restaurant)
+    @restaurant.user_id = current_user.id
+      if restaurant.save
+        redirect_to restaurants_path
+      else
+        render :new
+      end
   end
 
   def edit
@@ -22,5 +34,18 @@ class RestaurantsController < ApplicationController
 
   def delete
   end
+
+private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def params_restaurant
+    params.require(:cat).permit(:user_id, :category, :address, :city, :zip_code, :picture, :phone_number,
+     :iban, :address, :picture, :latitude, :longitude)
+  end
+
+
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
+
+
 end
