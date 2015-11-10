497194de6c3fae6fa71a17190fe8e2158855dcde
boris hacker <hacker.boris@gmail.com>
2015-11-10 14:32:34 +0100

    footer

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151110102255) do
-=======
-ActiveRecord::Schema.define(version: 20151110101233) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
