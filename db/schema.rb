9ce1e39d2ef94ab1dab9f6a6e0a75544e5b778a3
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-11-10 11:18:31 +0100

    restaurant validations and autocompletion done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109212749) do
+ActiveRecord::Schema.define(version: 20151110101233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20151109212749) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.string   "street_number"
+    t.string   "route"
   end
 
   add_index "restaurants", ["user_id"], name: "index_restaurants_on_user_id", using: :btree
