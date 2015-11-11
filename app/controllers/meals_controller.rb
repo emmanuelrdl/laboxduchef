932d2677e2c7570e6f3bb0ae6d43d4d3392b5b26
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-11-11 07:59:42 +0100

    fixing issue

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,7 +2,7 @@ class MealsController < ApplicationController
 
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_meal, only: [:show, :edit, :update, :destroy]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy, :create]
 
   def index
     @meals = Meal.all.order('created_at DESC').page(params[:page])
@@ -19,8 +19,7 @@ class MealsController < ApplicationController
   end
 
   def create
-    @meal = Meal.new(params_meal)
-    restaurant_id = Restaurant.find(params[:id])
+    @meal = @restaurant.meals.create(params_meal)
     if @meal.save
       redirect_to restaurant_meals_path(@meal)
     else
@@ -52,13 +51,13 @@ class MealsController < ApplicationController
 
   private
 
-  def set_meal
+  def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @meal = @restaurant.meals.find(:meal_id)
+
   end
 
   def params_meal
-    params.require(:meal).permit(:name, :description, :price, :quantity, :picture, :starting_date, :take_away_noon_starts_at, :take_away_evening_starts_at, :take_away_noon_ends_at, :take_away_evening_ends_at)
+    params.require(:meal).permit(:name, :description, :price, :quantity, :picture, :starting_date, :take_away_noon_starts_at, :take_away_evening_starts_at, :take_away_noon_ends_at, :take_away_evening_ends_at, :restaurant_id)
   end
 
 end
