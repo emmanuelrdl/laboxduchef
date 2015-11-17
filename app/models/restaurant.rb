36cd30582d9777e22d2edbc83fd5cf479c096e58
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-11-17 20:09:05 +0100

    cart show done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,7 @@ class Restaurant < ActiveRecord::Base
   has_many :meals, dependent: :destroy
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" }
+  has_many :orders
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
