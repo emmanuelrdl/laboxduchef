3fd201c853e997751f7353e0b4d00546f170ba66
Camille Anelli <camilleanelli32@gmail.com>
2015-11-18 16:27:44 +0100

    add maps to order show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,9 @@ class UsersController < ApplicationController
   before_action :set_user, only: [ :show ]
 
   def show
-    @last_order = current_user.orders.last
+    @last_order = current_user.orders.where(status: 'paid').last
     @restaurants = current_user.restaurants
-    @paid_orders = Order.where(status: 'paid')
-
-    @meal = Meal.find(params[:id])
-    @restaurant_coordinates = [{ lat: @last_order.order_meals.first.meal.restaurant.latitude, lng: @last_order.order_meals.first.meal.restaurant.longitude }]
   end
-
 
   private
 
