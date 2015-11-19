8a16f99310f383fe6f2958eed57480b4ef3bae0e
Camille Anelli <camilleanelli32@gmail.com>
2015-11-19 16:17:29 +0100

    center the payment new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,12 @@ class ApplicationController < ActionController::Base
   end
 
 
+  def default_url_options
+  if Rails.env.production?
+    { host: 'laboxduchef.herokuapp.com' }
+  else
+    { host: ENV['HOST'] || 'localhost:3000' }
+  end
+end
+
 end
