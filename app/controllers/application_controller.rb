9ddbd86cf77b48d6eca48c0dec8ebeaf5dd22ecc
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-11-29 20:11:17 +0100

    fixing issue with meta image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
+def default_url_options
+  if Rails.env.production?
+    { host: 'laboxduchef.herokuapp.com' }
+  else
+    { host: ENV['HOST'] || 'localhost:3000' }
+  end
+end
+
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
