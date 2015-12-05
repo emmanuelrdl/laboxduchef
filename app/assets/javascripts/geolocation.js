7c30b46cdb76baa0b47510e604e54e904285d0d9
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-12-05 06:15:18 +0100

    working on map

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,31 +1,51 @@
-var x = document.getElementById("demo");
-
-function getLocation() {
+    // GeoLocation
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        navigator.geolocation.getCurrentPosition(function(position){
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            //console.log('latitude: '+latitude+' longitude: '+longitude);
+            geocodeLatLng(latitude, longitude);
+        }, function(error){
+            console.log(error);
+        });
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        console.log('Geolocation is not enabled.');
     }
-}
 
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
-
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
+    // Start - Reverse Geocoding
+    function geocodeLatLng(latitude, longitude) {
+        var geocoder = new google.maps.Geocoder;
+        var latlng = {lat: parseFloat(latitude), lng: parseFloat(longitude)};
+        //var latlng = {lat: 40.731, lng: -73.997};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                if(results[0]) {
+                    //console.log(results[0].address_components);
+                    var address = results[0].address_components;
+                    var location = '';
+                    var locality;
+                    for(var i=0; i<address.length; i++)
+                        if(address[i].types[0]=='sublocality_level_1')
+                            location += address[i].long_name + ', ';
+                        else if(address[i].types[0]=='locality') {
+                            location += address[i].long_name + ', ';
+                            locality = address[i].long_name;
+                        }
+                        else if(address[i].types[0]=='administrative_area_level_1' && address[i].short_name!=locality)
+                            location += address[i].long_name + ', ';
+                        else if(address[i].types[0]=='country') {
+                            location += address[i].long_name;
+                            $scope.country_code = address[i].short_name;
+                        }
+                    $scope.location = location;
+                    console.log($scope.location);
+                    console.log($scope.country_code);
+                }
+                else
+                    console.log('No results found');
+            } else {
+                console.log('Geocoder failed due to: ' + status);
+            }
+        });
     }
-}
+    // End - Reverse Geocoding
