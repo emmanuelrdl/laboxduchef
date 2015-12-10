f86764fa0b099f5533fee6381788581da5c3aac1
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-12-10 03:21:20 +0100

    working newsletter

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,8 +1,10 @@
 class NewslettersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
+  before_action :landing_page
 
  def new
     @newsletter = Newsletter.new
-  end
+ end
 
 
   def create
@@ -13,6 +15,10 @@ class NewslettersController < ApplicationController
     else
       render :new
      end
+  end
+
+  def landing_page
+  @disable_nav = true
   end
 
 private
