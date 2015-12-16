d78432c5517d23c59740cf172900d44e462f4a92
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-12-16 18:15:02 +0100

    working on front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
     @meals = Meal.all
-    @restaurant = Restaurant.all
+    @restaurants = Restaurant.all
+
+    @markers = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
+    @my_restaurant = restaurant
+    marker.lat restaurant.latitude
+    marker.lng restaurant.longitude
+    # marker.infowindow render_to_string(:partial => "/meals/infowindow")
+    marker.infowindow render_to_string(:partial => 'pages/infowindow', :locals => { :object => @my_restaurant})
+    end
   end
 
   def espace_restaurants
