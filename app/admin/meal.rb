a171aa251d0c7f71efb963d2bbf5a0bfcf03de65
emmanuel <emmanuel.rudelle@hotmail.fr>
2016-01-07 12:41:35 +0100

    owkring on active admin

--- app/admin/meal.rb
+++ app/admin/meal.rb
@@ -3,8 +3,9 @@ ActiveAdmin.register Meal do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
-#
+permit_params :restaurant_id, :name, :quantity, :description, :starting_date, :second_date,
+:picture_file_name, :picure_content_type, :picture_file_size, :picture_updated_at, :active,
+:price_cents, :stock, :seated_price, :take_away_noon, :take_away_evening
 # or
 #
 # permit_params do
