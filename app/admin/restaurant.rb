d2d4d4188456d08f0b7f9f6670345c8b96bb0919
emmanuel <emmanuel.rudelle@hotmail.fr>
2016-01-07 12:19:43 +0100

    working on active admin

--- app/admin/restaurant.rb
+++ app/admin/restaurant.rb
@@ -3,7 +3,10 @@ ActiveAdmin.register Restaurant do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+ permit_params :user_id, :name, :locality, :postal_code, :phone_number, :latitude,
+ :longitude, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at,
+ :street, :confirmed, :iban, :take_away_noon_starts_at, :take_away_noon_ends_at, :take_evening_starts_at,
+ :take_away_evening_ends_at, :id , :commit
 #
 # or
 #
