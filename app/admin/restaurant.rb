f3910e6e5d23830194432a57929c52e272554c5a
emmanuel <emmanuel.rudelle@hotmail.fr>
2016-01-07 12:27:26 +0100

    working on active admin

--- app/admin/restaurant.rb
+++ app/admin/restaurant.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Restaurant do
 #
  permit_params :user_id, :name, :locality, :postal_code, :phone_number, :latitude,
  :longitude, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at,
- :street, :confirmed, :iban, :take_away_noon_starts_at, :take_away_noon_ends_at, :take_evening_starts_at,
+ :street, :confirmed, :iban, :take_away_noon_starts_at, :take_away_noon_ends_at, :take_away_evening_starts_at,
  :take_away_evening_ends_at, :id , :commit
 #
 # or
