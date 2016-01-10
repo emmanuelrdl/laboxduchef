473299fb5bcc29376d1dd0b5576bdf6caa9d62c2
emmanuel <emmanuel.rudelle@hotmail.fr>
2016-01-10 15:30:52 +0100

    working on fb connect

--- db/migrate/20160110142144_remove_column_from_user.rb
+++ db/migrate/20160110142144_remove_column_from_user.rb
@@ -1,8 +1,4 @@
 class RemoveColumnFromUser < ActiveRecord::Migration
   def change
-     remove_column :users, :token_expiry, :datetime
-     remove_column :users, :token, :string
-     remove_column :users, :uid, :string
-     remove_column :users, :provider, :string
   end
 end
