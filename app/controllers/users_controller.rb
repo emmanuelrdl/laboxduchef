4e45699d3864883c821c5c09c6d3035bf39cff94
emmanuel <emmanuel.rudelle@hotmail.fr>
2016-01-27 12:17:07 +0100

    working on debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,31 @@ class UsersController < ApplicationController
 
 
   def show
-    @last_order = current_user.orders.where(status: 'confirmed' || 'paid').last
-    @restaurants = current_user.restaurants
     if current_user.restaurant_owner
+      @restaurants = current_user.restaurants
       @restaurant = current_user.restaurants.first
       @meals = @restaurant.meals
-    elsif current_user.orders.where(status:'confirmed' || 'paid').count >= 1 && current_user.orders.last.status == 'confirmed' || 'paid'
-      @last_order = current_user.orders.where(status: 'confirmed' || 'paid').last
-      @restaurant_full_address = current_user.orders.where(status:"confirmed" || 'paid').last.meal.restaurant.full_address
+    else
+      if current_user.orders >= 1
+        if current_user.orders.last.status == 'paid' || current_user.orders.last.status == 'confirmed'
+          @last_order = current_user.orders.where(status: 'paid').last || current_user.orders.where(status: 'confirmed').last
+          @restaurant_full_address = @last_order.meal.restaurant.full_address
+        elsif current_user.orders.last.status == "cart" || current_user.orders.last.status == "cancelled"
+          if current_user.orders.where(status:"confirmed" || 'paid').count >= 1
+             @last_order = current_user.orders.where(status:'paid' || 'confirmed').last
+          end
+        end
+      end
     end
   end
+
+
+
+  private
 
   def navbar_choice
     @navbar_other = true
   end
-
-  private
 
   def set_user
     @user = User.find(params[:id])
