763ae3005e058235c42309a81a996bdac6262a47
emmanuel <emmanuel.rudelle@hotmail.fr>
2016-02-02 15:05:50 +0100

    working on image upload

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,12 +1,12 @@
 // check the value of meal and restaurant picture
 
-// document.getElementById("meal_picture").onchange = function () {
-// document.getElementById("uploadFile").value = this.value;
+//  document.getElementById("meal_picture").onchange = function () {
+//  document.getElementById("uploadFile").value = this.value;
 // };
 
 
-// document.getElementById("restaurant_picture").onclick = function () {
-// document.getElementById("uploadFile").value = this.value;
-// };
+document.getElementById("upload").onchange = function () {
+document.getElementById("uploadFile").value = this.value;
+};
 
 // check the value of meal and restaurant picture
