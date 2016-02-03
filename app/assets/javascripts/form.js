fdd8fbb50896e0ebadcb71909426f08af9f9d839
emmanuel <emmanuel.rudelle@hotmail.fr>
2016-02-03 09:52:06 +0100

    working on on change event

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -6,7 +6,11 @@
 
 
 // document.getElementById("upload").onchange = function () {
-// document.getElementById("uploadFile").value = this.value;
+
 // };
 
 // check the value of meal and restaurant picture
+
+$(document).on('change', '#upload', function() {
+document.getElementById("uploadFile").value = this.value;
+});
