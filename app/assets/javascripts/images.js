eea470178dec6915cd47fa078ec6f794203c8287
emmanuel <emmanuel.rudelle@hotmail.fr>
2016-02-09 13:40:22 +0100

    working on fronté

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,8 +1,10 @@
 
-  $(document).ready(function () {
-      $('.btn-waiting').click(function() {
-      $('#modal_waiting').modal('show');
-        });
-  });
+$(document).ready(function () {
+    $('.btn-waiting').click(function() {
+    $('#modal_waiting').modal('show');
+      });
+});
 
-
+$(window).load(function(){
+  $('#modal_waiting').modal('hide');
+});
